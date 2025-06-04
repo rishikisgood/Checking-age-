744d55d4fecfbdba581dd17b000828d406ff1d37
rishikisgood <Kaulrishik15@gmail.com>
2025-06-04 14:20:49 +0530

    Add files via upload

---
+++ Checking Age.py
@@ -0,0 +1,11 @@
+age=int(input("Please Enter Your Age: "))
+
+if age > 20:
+    print("You are Elder than 20 years of age")
+else:
+    print("You are Younger than 20 years of age") 
+
+if age < 10:
+    print("You are younger than 10 years of age")
+else:
+    print("You are elder than 10 years of age TOO")
